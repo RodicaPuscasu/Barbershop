280c9ed6da261bdef6e45469dd459e02cf42a353
RodicaPuscasu <150557402+RodicaPuscasu@users.noreply.github.com>
2024-01-25 07:37:26 +0200

    lesson 14 part 3

--- js/index.js
+++ js/index.js
@@ -38,3 +38,18 @@ const onBack = () => {
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-menu-open]"),
+    closeModalBtn: document.querySelector("[data-menu-close]"),
+    modal: document.querySelector("[data-menu]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
